17c1a2bfa131f525cbd0d5e705d3427bcc7e73d3
robotonist <93608897+Robotonist@users.noreply.github.com>
2022-05-12 21:18:47 -0700

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
 THRESHOLD=70
+EMAIL=yourname.yourlastname@company.com
 
 if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-	    mail -s 'Disk Space Alert' YOUR.EMAIL@HERE.com << EOF
+	    mail -s 'Disk Space Alert' $EMAIL << EOF
 Your root partition remaining free space is critically low. Used: $CURRENT%
 EOF
 fi
